f11e5e129bee33f942207a89539622922265579e
Rafif Farras <rafiffarras@macbook-air.local>
2024-12-06 23:09:09 +0700

    feat: add car page (minus preview saja)

---
+++ src/frontend/page/car/add-car/add_car.js
@@ -0,0 +1,8 @@
+function showpreview (event){
+    if (event. target.files.length > 0){
+    var src = URL.createobjectURL(event.target.files[®]);
+    var preview = document.getElementById("file-ip-1-preview");
+    preview. src = src;
+    preview. style.display = "block";
+    }
+}
