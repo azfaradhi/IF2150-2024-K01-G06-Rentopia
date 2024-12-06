1b79547d29f3797218828402813e1c93962b21bf
Barru Adi <barru.adi@gmail.com>
2024-12-06 21:36:16 +0700

    feat: database api

--- src/frontend/preload.js
+++ src/frontend/preload.js
@@ -9,4 +9,8 @@ contextBridge.exposeInMainWorld('api', {
         const response = await fetch(`http://127.0.0.1:5000/api/activity/${id_activity}`);
         return response.json();
     },
+    showActivity: async (id_activity) => {
+        const response = await fetch(`http://127.0.0.1:5000/api/activity/show/${id_activity}`);
+        return response.json();
+    },
 });
