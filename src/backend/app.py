0d80a4527496a31cf86c8b6dfb3cca5f359787b5
azfaradhi <aradhihakim@gmail.com>
2024-12-08 00:47:53 +0700

    feat: adding blueprint for car and customer

--- src/backend/app.py
+++ src/backend/app.py
@@ -4,6 +4,8 @@
 from flask import Flask, jsonify
 # from .page import Activity
 from controllers.activity_controller import activity_bp
+from controllers.car_controller import car_bp
+from controllers.customer_controller import customer_bp
 # from controllers.activity_controller import activity_bp
 # from page.activity.activity_model import Activity
 # from controllers.activity_controller import *
@@ -12,6 +14,8 @@ app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(activity_bp)
+app.register_blueprint(car_bp)
+app.register_blueprint(customer_bp)
 
 @app.route('/')
 def home():
