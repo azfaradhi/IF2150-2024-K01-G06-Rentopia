00f036743d51f6f8bd88a2e6d71ca3e5e31bfb4c
azfaradhi <aradhihakim@gmail.com>
2024-12-08 00:56:04 +0700

    fix: removing unused module in init

--- src/backend/page/__init__.py
+++ src/backend/page/__init__.py
@@ -2,4 +2,3 @@ from .activity.activity_model import *
 from .car.car_model import *
 from .customer.customer_model import *
 from .notification.notification_model import *
-from .report.report_model import *
