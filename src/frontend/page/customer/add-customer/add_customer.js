a440192342120b8841097beb37260e0b639d94a4
Rafif Farras <rafiffarras@macbook-air.local>
2024-12-08 19:30:54 +0700

    fix: localhost to 127.0.0.2 (ada yg ketinggalan 1 tdi ges maap)

--- src/frontend/page/customer/add-customer/add_customer.js
+++ src/frontend/page/customer/add-customer/add_customer.js
@@ -29,7 +29,7 @@ async function  initAddCustomer(params) {
                 };
                 console.log("Collect data customer: ", customerData);
                 try{
-                    const response = await fetch('http://localhost:5000/api/customer/create', {
+                    const response = await fetch('http://127.0.0.1:5000/api/customer/create', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
