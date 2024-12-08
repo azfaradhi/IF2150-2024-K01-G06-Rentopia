a1a800c4e4e08088676af78fd9606f7e17bde4f4
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-12-08 21:31:54 +0700

    feat: customer controller for update data
    and fix create with handling error for empty non-null field

--- src/backend/controllers/customer_controller.py
+++ src/backend/controllers/customer_controller.py
@@ -19,7 +19,19 @@ def get_customer(id_cust):
 @customer_bp.route('/api/customer', methods=['POST'])
 def create_customer():
     data = request.json
+<<<<<<< Updated upstream
 
+=======
+    print("Recevied data: ", data)
+    
+    fields = ['id_cust', 'name_cust', 'phone_cust', 'address_cust']
+    
+    for field in fields: 
+        print("cek")
+        if not data or field not in data or data[field] is None or data[field] == '':
+            return jsonify({'error': f'Missing required field: {field}'}), 400
+    
+>>>>>>> Stashed changes
     customer = Customer(data['id_cust'])
     customer.id_cust = data['id_cust']
     customer.name_cust = data['name_cust']
@@ -40,4 +52,46 @@ def show_customer(id_cust):
         'address_cust': customer.address_cust,
         'additional_info_cust': customer.additional_info_cust,
         'status_cust': customer.status_cust
+<<<<<<< Updated upstream
     })
+=======
+    })
+
+@customer_bp.route('/api/customer/alldata', methods=['GET'])
+def get_customer_pagination():
+    page = int(request.args.get('page', 1))
+    items_per_page = int(request.args.get('items_per_page', 10))
+    
+    customers_list, total_customers, total_pages = Customer.get_paginated_customers(page, items_per_page)
+    
+    return jsonify({
+        'page': page,
+        'items_per_page': items_per_page,
+        'total_pages': total_pages,
+        'total_customers': total_customers,
+        'customers': customers_list
+    })
+    
+@customer_bp.route('/api/customer/update', methods=['POST'])
+def update_customer():
+    data = request.json
+    print("Recevied data: ", data)
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+    
+    fields = ['id_cust', 'name_cust', 'phone_cust', 'address_cust']
+    
+    for field in fields: 
+        if field not in data or data[field] is None or data[field] == '':
+            return jsonify({'error': f'Missing required field: {field}'}), 400
+    
+    customer = Customer(data['id_cust'])
+    customer.setIDCustomer(int(data['id_cust']))
+    customer.setNameCustomer(data['name_cust'])
+    customer.setPhoneCustomer(data['phone_cust'])
+    customer.setAddressCustomer(data['address_cust'])
+    customer.setAdditionalInfoCustomer("")
+    customer.setStatusCustomer("inactive")
+    customer.saveCustomer()
+    return jsonify({'message': 'Customer updated successfully'})
+>>>>>>> Stashed changes
