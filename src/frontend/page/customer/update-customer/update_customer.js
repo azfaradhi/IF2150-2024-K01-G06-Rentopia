a6c4d9f9c7b30137211b7ea8bd1114c19660da45
Barru Adi <barru.adi@gmail.com>
2024-12-09 12:03:10 +0700

    feat: update customer w/o edgecase

--- src/frontend/page/customer/update-customer/update_customer.js
+++ src/frontend/page/customer/update-customer/update_customer.js
@@ -1,5 +1,7 @@
 console.log("apdet cust");
 
+let custData = null;
+
 async function initUpdateCustomer(id_cust) {
     console.log("apdet custttttt");
     const placeholder = await fetch(`http://127.0.0.1:5000/api/customer/${id_cust}`);
@@ -22,5 +24,56 @@ async function initUpdateCustomer(id_cust) {
             const name = document.getElementById("cust-name").value;
             const phone = document.getElementById("cust-phone").value;
             const address = document.getElementById("cust-address").value;
+
+            let valid = true;
+            if (!nik || !name || !phone || !address){
+                valid = false;
+                alert("Please fill in all fields!");
+                document.getElementById("cust-nik").value = "";
+                document.getElementById("cust-name").value = "";
+                document.getElementById("cust-phone").value = "";
+                document.getElementById("cust-address").value = "";   
+            }
+
+            if (valid) {
+                const custData = {
+                    id_cust: nik,
+                    name_cust: name,
+                    phone_cust: phone,
+                    address_cust: address,
+                }
+                console.log("Collect data customer: ", custData);
+                try {
+                    const passingData = await fetch('http://127.0.0.1:5000/api/customer/update', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(custData),
+                    });
+
+                    if (!passingData.ok) {
+                        throw new Error(`Failed to update customer: ${passingData.statusText}`);
+                    }
+                    const result = await passingData.json();
+                    console.log("Server Response: ", result.message);
+                    document.getElementById("cust-nik").value = "";
+                    document.getElementById("cust-name").value = "";
+                    document.getElementById("cust-phone").value = "";
+                    document.getElementById("cust-address").value = "";
+                    alert("Success updating customer!");
+                    window.location.hash = '/customer';
+                } catch (error) {
+                    console.error("Detailed error: ", error);
+                }  
+            }
+        });
+    }
+
+    if (cancelButton) {
+        cancelButton.addEventListener('click', () => {
+            console.log("Cancel");
+            window.location.hash = "/customer";
+        })
     }
 }
